03b5812d86542fcadee644186aa215fc390696ff
githubfoam <lequestionis@gmail.com>
2020-06-20 12:09:55 +0300

    Полице́йские извлекли́ её из маши́ны.

--- scripts/freebsd.sh
+++ scripts/freebsd.sh
@@ -2,8 +2,6 @@
 set -eox pipefail #safety for script
 echo "=========================================================================================="
 echo $SHELL # freebsd shell
-ls /
-ls /tmp/vagrant
 echo "===================python virtual environment======================================================================="
 sudo pkg install --yes --quiet python36 py36-pip
 sudo pip-3.6 install virtualenv
@@ -13,7 +11,22 @@ virtualenv /home/vagrant/venvansible
 . /home/vagrant/venvansible/bin/activate
 pip install ansible
 ansible --version
-udo pip freeze > /tmp/vagrant/requirements.txt
+cat <<EOF | sudo tee ansible-local.yml
+---
+  - name: "Run ansible locally localhost"
+    hosts: localhost
+    connection: local
+    tasks:
+
+    - name: "run ls -lrt command"
+      shell: "ls -lrt"
+      register: "output"
+
+    - debug: var=output.stdout_lines
+EOF
+pwd && ls -lai
+ansible-playbook ansible-local.yml
+sudo pip freeze > /tmp/vagrant/requirements.txt
 cat /tmp/vagrant/requirements.txt
 deactivate
 echo "===================ansible======================================================================="
