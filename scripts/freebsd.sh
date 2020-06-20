e69f30158b7530344fcef078cdf5f21225043316
githubfoam <lequestionis@gmail.com>
2020-06-20 11:52:19 +0300

    Её лицо́ создаёт ло́жное представле́ние

--- scripts/freebsd.sh
+++ scripts/freebsd.sh
@@ -2,4 +2,22 @@
 set -eox pipefail #safety for script
 echo "=========================================================================================="
 echo $SHELL # freebsd shell
+ls /
+ls /tmp/vagrant
+echo "===================python virtual environment======================================================================="
+sudo pkg install --yes --quiet python36 py36-pip
+sudo pip-3.6 install virtualenv
+which python3.6
+virtualenv --version
+virtualenv /home/vagrant/venvansible
+. /home/vagrant/venvansible/bin/activate
+pip install ansible
+ansible --version
+udo pip freeze > /tmp/vagrant/requirements.txt
+cat /tmp/vagrant/requirements.txt
+deactivate
+echo "===================ansible======================================================================="
+pkg search ansible
+sudo pkg install --yes --quiet py36-ansible-2.8.5
+ansible --version
 echo "=========================================================================================="
