7c00ffecfb3efe33c1f871a598fd4c3a3ae320d2
githubfoam <lequestionis@gmail.com>
2020-08-24 19:17:30 +0300

    init dragonfly

--- Makefile
+++ Makefile
@@ -1,8 +1,16 @@
-IMAGE := alpine/fio
-APP:="scripts/deploy-vagrant.sh"
+deploy-dragonflybsd-hammer:
+	bash scripts/deploy-dragonflybsd-hammer.sh
+
+deploy-dragonflybsd-generic:
+	bash scripts/deploy-dragonflybsd-generic.sh
+
+deploy-dragonflybsd:
+	bash scripts/deploy-dragonflybsd.sh
 
 deploy-vagrant:
 	bash scripts/deploy-vagrant.sh
-push-image:
-	docker push $(IMAGE)
-.PHONY: deploy-vagrant push-image
+
+deploy-libvirt:
+	bash scripts/deploy-libvirt.sh
+
+.PHONY: deploy-vagrant deploy-dragonflybsd
--- scripts/deploy-vagrant.sh
+++ scripts/deploy-vagrant.sh
@@ -1,30 +1,34 @@
 #!/bin/bash
-set -eox pipefail #safety for script
+set -o errexit
+set -o pipefail
+set -o nounset
+set -o xtrace
+# set -eox pipefail #safety for script
 
-echo "=============================Install kvm qemu libvirt============================================================="
-apt-get -qq update
-apt-get install -y cpu-checker bridge-utils dnsmasq-base ebtables libvirt-bin libvirt-dev qemu-kvm qemu-utils ruby-dev
-systemctl status libvirtd
-libvirtd --version
-egrep -c '(vmx|svm)' /proc/cpuinfo #If 0 it means that your CPU doesn't support hardware virtualization.If 1 or more it does - but you still need to make sure that virtualization is enabled in the BIOS.
-addgroup libvirtd
-adduser  $(id -un) libvirtd #ensure that your username is added to the group libvirtd
-kvm-ok
 echo "=============================Install Vagrant============================================================="
-export VAGRANT_CURRENT_VERSION="$(curl -s https://checkpoint-api.hashicorp.com/v1/check/vagrant | jq -r -M '.current_version')"
+# export VAGRANT_CURRENT_VERSION="2.2.9"
+apt-get update -yqq
 apt-get install -qqy unzip jq
-wget -nv https://releases.hashicorp.com/vagrant/${VAGRANT_CURRENT_VERSION}/vagrant_${VAGRANT_CURRENT_VERSION}_x86_64.deb
+
+# https://releases.hashicorp.com/vagrant/2.2.9/vagrant_2.2.9_SHA256SUMS
+export VAGRANT_CURRENT_VERSION="$(curl -s https://checkpoint-api.hashicorp.com/v1/check/vagrant | jq -r -M '.current_version')"
+export VAGRANT_URL="https://releases.hashicorp.com/vagrant/${VAGRANT_CURRENT_VERSION}/vagrant_${VAGRANT_CURRENT_VERSION}_x86_64.deb"
+export VAGRANT_SHA256_URL="https://releases.hashicorp.com/vagrant/$VAGRANT_CURRENT_VERSION/vagrant_${VAGRANT_CURRENT_VERSION}_SHA256SUMS"
+export  VAGRANT_SHA256_SIG_URL="https://releases.hashicorp.com/vagrant/$VAGRANT_CURRENT_VERSION/vagrant_${VAGRANT_CURRENT_VERSION}_SHA256SUMS.sig"
+
+# wget -nv "${VAGRANT_URL}"
+curl -LO "${VAGRANT_URL}"
+curl -LO "${VAGRANT_SHA256_URL}"
+curl -LO "${VAGRANT_SHA256_SIG_URL}"
+export HASHICORP_PUBLIC_KEY_URL="https://keybase.io/hashicorp/pgp_keys.asc" #https://www.hashicorp.com/security
+# curl -sSL "${HASHICORP_PUBLIC_KEY_URL}" | gpg --import - # import the public key (PGP key)
+`curl -sSL "${HASHICORP_PUBLIC_KEY_URL}" | gpg --import -` # import the public key (PGP key)
+gpg --verify "vagrant_${VAGRANT_CURRENT_VERSION}_SHA256SUMS.sig" "vagrant_${VAGRANT_CURRENT_VERSION}_SHA256SUMS" 2>/dev/null #Verify the signature file is untampered
+
+sha256sum  vagrant_${VAGRANT_CURRENT_VERSION}_x86_64.deb # via sha256sum
+openssl dgst -sha256 vagrant_${VAGRANT_CURRENT_VERSION}_x86_64.deb # via openssl
+
+echo $(shasum -a 256 -c "vagrant_${VAGRANT_CURRENT_VERSION}_SHA256SUMS" 2>&1 | grep OK)
+
 dpkg -i vagrant_${VAGRANT_CURRENT_VERSION}_x86_64.deb
 vagrant version
-echo "=============================Install Vagrant============================================================="
-vagrant plugin install vagrant-libvirt #The vagrant-libvirt plugin is required when using KVM on Linux
-vagrant plugin install vagrant-mutate #Convert vagrant boxes to work with different providers
-vagrant box add "freebsd/FreeBSD-12.1-STABLE" --provider=virtualbox
-vagrant mutate "freebsd/FreeBSD-12.1-STABLE" libvirt
-vagrant status
-virsh list --all #show all running KVM/libvirt VMs
-vagrant box list #veridy installed boxes
-vagrant status #Check the status of the VMs to see that none of them have been created yet
-vagrant up --provider=libvirt node-4
-vagrant status
-virsh list --all #show all running KVM/libvirt VMs
